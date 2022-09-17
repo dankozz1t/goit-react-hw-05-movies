7a0e2e55cd2cd9e37cc85d328fb8306296f98c9e
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-17 20:46:41 +0300

    fix import

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { api } from 'service/api.service';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
