79a6db471caef652af0c2ee10b1c5f9d999b2fe1
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-17 15:54:02 +0300

    delete basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,8 +12,8 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
