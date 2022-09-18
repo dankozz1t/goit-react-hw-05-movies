f25eba91941b1fd15492a6e5fccd4aac1009627d
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-18 10:54:51 +0300

    fix path with https

--- src/helper/pathImageStabilization.js
+++ src/helper/pathImageStabilization.js
@@ -3,8 +3,16 @@ export const pathImageStabilization = pathImage => {
     return 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
   }
 
+  if (pathImage.includes('https')) {
+    return pathImage.slice(1, pathImage.length);
+  }
+
   if (pathImage[0] === '/') {
     return `https://image.tmdb.org/t/p/w500${pathImage}`;
+  }
+
+  if (pathImage.includes('https')) {
+    return pathImage.slice(1, pathImage.length);
   }
 
   return `https://image.tmdb.org/t/p/w500/${pathImage}`;
