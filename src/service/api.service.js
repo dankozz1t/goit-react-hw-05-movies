e85f0249f6eade76be0e475f6f1bc6186e605eb5
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-19 12:36:49 +0300

    delete async

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -6,16 +6,16 @@ const instance = axios.create({
     api_key: 'd7ee9dda466bc4ced4432fb2e147fc44',
   },
 });
-const fetchFilms = async () => await instance.get(`/trending/movie/day`);
+const fetchFilms = () => instance.get(`/trending/movie/day`);
 
-const fetchFilmsByName = async query =>
-  await instance.get(`/search/movie`, { params: { query } });
+const fetchFilmsByName = query =>
+  instance.get(`/search/movie`, { params: { query } });
 
-const fetchFilmById = async id => await instance.get(`/movie/${id}`);
+const fetchFilmById = id => instance.get(`/movie/${id}`);
 
-const fetchFilmCast = async id => await instance.get(`/movie/${id}/credits`);
+const fetchFilmCast = id => instance.get(`/movie/${id}/credits`);
 
-const fetchFilmReviews = async id => await instance.get(`/movie/${id}/reviews`);
+const fetchFilmReviews = id => instance.get(`/movie/${id}/reviews`);
 
 export const api = {
   fetchFilms,
